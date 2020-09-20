b695f4b11d9fde65af1cce0dac31026ad86b5c09
vaibhav <vvaibhav3@vw>
2020-09-20 15:29:45 +0530

    part 1

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {Navbar, NavbarBrand,Button} from 'reactstrap';
+import Menu from './components/MenuComponent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Vaibhav
-        </a>
-      </header>
+    <div >
+      <Navbar color="light" expand="md">
+        <div className="container">
+            <NavbarBrand href="/">FREE FOOD</NavbarBrand>
+            <Button color="primary" outline size="sm">About</Button>
+        </div>
+      </Navbar>
+      <Menu/>
     </div>
   );
 }
